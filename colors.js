4d81913b0aa176e894512cc7c3810a84f66bc39d
lujehle <lukas-jehle@web.de>
2025-05-08 17:34:18 +0200

    Colors für colorbar hinzugefügt

---
+++ colors.js
@@ -0,0 +1,17 @@
+const COLORS = {
+    temperature: [
+        {min: -100, max: -25, color: "#9f80ff"},
+        {min: -100, max: -25, color: "#784cff"},
+        {min: -100, max: -25, color: "#0f5abe"},
+        {min: -100, max: -25, color: "#1380ff"},
+        {min: -100, max: -25, color: "#19cdff"},
+        {min: -100, max: -25, color: "#8fffff"},
+        {min: -100, max: -25, color: "#b0ffbc"},
+        {min: -100, max: -25, color: "#ffff73"},
+        {min: -100, max: -25, color: "#ffbe7d"},
+        {min: -100, max: -25, color: "#ff9b41"},
+        {min: -100, max: -25, color: "#ff5a41"},
+        {min: -100, max: -25, color: "#ff1e23"},
+        {min: -100, max: -25, color: "#fa3c96"},
+    ]
+};
